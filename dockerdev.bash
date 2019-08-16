af9ea869873469618b2233f5a36ae8da0e3f4c8e
Brian DuSell <bdusell@gmail.com>
2019-08-16 14:58:39 -0400

    Add more comments to the head of the script.

--- dockerdev.bash
+++ dockerdev.bash
@@ -1,8 +1,40 @@
+# MIT License
+#
+# Copyright (c) 2019 Brian DuSell
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# ---
+#
+# dockerdev.bash
+#
+# A library of bash functions to facilitate using Docker containers as local
+# development environments.
+#
+# Repository: https://github.com/bdusell/dockerdev
+# Report bugs at: https://github.com/bdusell/dockerdev/issues
+#
 # This file defines functions for managing Docker containers that are meant
 # to serve as local development environments. Typically the source code of
 # your application will be bind-mounted into the container so that you have
 # write access to the source code from within the container (which is useful
-# for things like lock files).
+# for things like package lock files).
 #
 # In addition to providing functions for managing single containers, this
 # file also includes functions for managing *stacks* of Docker containers
